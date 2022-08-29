a30894afee57cc8b8e92ee6136bbb5e58746e959
Magafurov <104352021+Magafurov@users.noreply.github.com>
2022-08-29 20:48:27 +0300

    commit

--- interface/start.bat
+++ interface/start.bat
@@ -1,3 +1,3 @@
 start "" "C:\Users\ti-ho\Documents\Programming\Git\chess_website\main.html"
-call conda activate ChessNN2
+:: call conda activate ChessNN2
 python C:\Users\ti-ho\Documents\Programming\Git\chess_website\python_backend.py
